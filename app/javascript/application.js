c3645918a54429e7fb147670fcdaaa3cac6d12cb
Peter Dumbari <dummytwiz@gmail.com>
2023-09-25 11:24:00 +0100

    added bootstrap to my application

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+import "popper";
+
+import "bootstrap";
+import "bootstrap/scss/functions";
+import "bootstrap/scss/variables";
+import "bootstrap/scss/variables-dark";
+import "bootstrap/scss/maps";
+import "bootstrap/scss/mixins";
+import "bootstrap/scss/utilities";
